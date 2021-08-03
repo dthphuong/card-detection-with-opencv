5c8bced7b9e356bd07a79554f77e977634901884
Duong Tran Ha Phuong <phuongduong@fpo.vn>
2021-08-03 20:29:04 +0700

    update algorithm

--- run.py
+++ run.py
@@ -4,7 +4,8 @@ import sys
 import numpy as np
 
 cv_version = cv2.__version__
-epsilon = 0.5
+rectangle_epsilon = 0.5
+position_epsilon = 0.25
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required = True, help = "Path to the input image")
@@ -12,7 +13,7 @@ ap.add_argument("-o", "--output", required = True, help = "Path to the output im
 args = vars(ap.parse_args())
 
 # Find the Largest Rectangle
-def findTheLargestRect(contours):
+def findTheLargestRect(contours, imageW, imageH):
     xMax = 0
     yMax = 0
     wMax = 0
@@ -20,14 +21,18 @@ def findTheLargestRect(contours):
 
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
-        if (w >= wMax and h >= hMax and h/w >= epsilon):
+        if (w >= wMax and h >= hMax and h/w >= rectangle_epsilon):
             wMax = w
             hMax = h
             xMax = x
             yMax = y
 
-    # print([xMax, yMax, wMax, hMax])
-    return [xMax, yMax, wMax, hMax]
+    print([xMax, yMax, wMax, hMax])
+    print(wMax/imageW)
+    if (wMax/imageW > position_epsilon):
+        return [xMax, yMax, wMax, hMax]
+    else:
+        return [0, 0, imageW, imageH]
 
 # Find contours
 def findContours(image):
@@ -53,7 +58,7 @@ def findContours(image):
     else:
         _, contours, hierarchy = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    return contours
+    return [contours, hierarchy]
 
 #================================================================================================
 # MAIN PROGRAM
@@ -64,11 +69,13 @@ if image is None:
     sys.exit("0")
 
 # cv2.imshow("Original", image)
+print((image.shape[0], image.shape[1]))
 
-contours = findContours(image)
+contours, hierarchy = findContours(image)
 # print(len(contours))
 
-[x, y, w, h] = findTheLargestRect(contours)
+[x, y, w, h] = findTheLargestRect(contours, image.shape[1], image.shape[0])
+print([x, y, w, h])
 
 # =====Crop by the largest contours=====
 cropImgage = image[y:y+h, x:x+w]
@@ -86,10 +93,11 @@ cv2.imwrite(args["output"], cropImgage)
 # =====Draw all contours=====
 # for i in range(len(contours)):
 #     if i == 0:
-#         print(contours[i])
+#         # print(contours[i])
 #         cv2.drawContours(drawing, contours, i, (0,0,255), 2, cv2.LINE_8, hierarchy, 0)
 #     else:
 #         cv2.drawContours(drawing, contours, i, (0,255,0), 2, cv2.LINE_8, hierarchy, 0)
+# cv2.imshow('all contours', drawing)
 
 # =====Draw contours bounding boxes=====
 # cv2.drawContours(image, contours, -1, (0,0,255), 3, cv2.LINE_8, hierarchy, 0)
@@ -97,5 +105,6 @@ cv2.imwrite(args["output"], cropImgage)
 # for bbox in bounding_boxes:
 #      [x , y, w, h] = bbox
 #      cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+# cv2.imshow('bounding boxes', image)
 
 # cv2.waitKey(0)
